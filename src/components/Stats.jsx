d8c3c7a2c3c50f43ef870f852f01ed3a5391a56a
ACE <anthonychimaobi43@gmail.co>
2024-07-01 06:07:32 +0000

    Feat:Billing, CTA, CardDeal, Clients, Testimonials,FeedBackCard,Stats,ffooter

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,17 +3,19 @@ import { stats } from '../constants'
 import styles from '../style'
 
 const Stats = () =>  (
-  <section className={`${styles.flexCenter} flex-row fle-wrap sm:mb-20 mb-6`}>
-    {stats.map((stat, index) => (
+  <section className={`${styles.flexCenter} border flex-row flex-wrap sm:mb-20 mb-6`}>
+    {stats.map((stat, index) => {
+      
+      return (
       <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`}> 
-        <h4 className="fony-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+        <h4 className={`fony-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white `}>
           {stat.value} 
         </h4>
-          <p className="fony-poppins font-normal xs:text-[20px] text-[1px] xs:leading-[26px] leading-[1px] text-gradient uppercase ml-3">
+          <p className={`fony-poppins font-normal xs:text-[20px] text-[1px] xs:leading-[26px] leading-[1px] text-gradient uppercase ml-3 `}>
             {stat.title}
           </p> 
       </div>
-    ))}
+    )})}
   </section>
   )
 
